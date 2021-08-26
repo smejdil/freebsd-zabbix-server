f3efc02d46458b21d1f7b349c281dc9db9dada63
Lukas Maly <smejdil@gmail.com>
2021-08-26 10:44:37 +0200

    Add commands for GCP

--- scripts/install-gcp.sh
+++ scripts/install-gcp.sh
@@ -3,12 +3,21 @@
 # ports
 portsnap fetch && portsnap extract
 
+# bash
+pkg install -y bash bash-completion
+
 # ansible
 pkg install -y py38-ansible python38 py38-setuptools
 ln -s /usr/local/bin/python3.8 /usr/bin/python
 
+# git
+pkg install -y git
+
 # make
 touch /etc/make.conf
 echo "BATCH=yes" > /etc/make.conf
+
+# get projekt
+git clone https://github.com/smejdil/freebsd-zabbix-server/
 
 # EOF
